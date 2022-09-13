cd471f494f87e8cdaea1eea4cb04690e460ecdee
akshat799 <gulati.akshat799@gmail.com>
2022-09-12 19:05:49 -0600

    part 9 and 10 done

--- get.py
+++ get.py
@@ -2,4 +2,4 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get("http://www.google.com/"))
+print(requests.get("https://raw.githubusercontent.com/akshat799/CMPUT404Lab1/main/get.py").text)
